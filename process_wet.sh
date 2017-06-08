6360aca92efadd1937750984a2e3d52e63665093
Tim Reichelt <s1547426@sms.ed.ac.uk>
2017-06-08 13:00:38 +0000

    Refactor collecting monolingual and deduping into functions

--- process_wet.sh
+++ process_wet.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# TODO: Usage description.
+# TODO: Make functions for monolingual and deduplication
+
 # Exit on error
 set -e
 set -o pipefail
@@ -18,11 +21,10 @@ DEDUPED_BIN=/home/tim/commoncrawl/dedupe.sh
 # Use -gt 0 to consume one or more arguments per pass in the loop (e.g.
 # some arguments don't have a corresponding value to go with it such
 # as in the --default example).
-# note: if this is set to -gt 0 the /etc/hosts part is not recognized ( may be a bug )
 while [[ $# -gt 1 ]]; do
     key="$1"
 
-    # TODO: Add explanations of characters.
+    # TODO: Add explanations of parameters.
     case $key in
         -min|--monolingual-in)
             MONOLINGUAL_IN="$2"
@@ -42,6 +44,7 @@ while [[ $# -gt 1 ]]; do
             ;;
         --sshloginfile)
             SSHLOGINFILE="$2"
+            shift # past argument
             ;;
         *)
             # unknown option
@@ -50,12 +53,7 @@ while [[ $# -gt 1 ]]; do
     shift # past argument or value
 done
 
-#echo $MONOLINGUAL_IN
-#echo $MONOLINGUAL_OUT
-#echo $DEDUPED_OUT
-#echo $LANGUAGESFILE
-#echo $SSHLOGINFILE
-#exit 0
+# TODO: Check wether all necessary command options are provided.
 
 if [[ ${SSHLOGINFILE} ]]; then
     PARALLEL_OPTIONS="--nice 19 --progress --sshloginfile ${SSHLOGINFILE} -j ${PARALLELJOBS}"
@@ -63,26 +61,29 @@ else
     PARALLEL_OPTIONS="--nice 19 --progress -j ${PARALLELJOBS}"
 fi
 
-COMMAND="
-ls --hide=wet.* ${MONOLINGUAL_IN} | \
-    parallel ${PARALLEL_OPTIONS} ${MONOLINGUAL_BIN} ${MONOLINGUAL_IN}/{} ${MONOLINGUAL_OUT}/{}
-"
+extract_monolingual() {
+    echo ""
+    echo "Extracting monolingual data.."
+    ls --hide=wet.* "${MONOLINGUAL_IN}" | \
+        parallel ${PARALLEL_OPTIONS} ${MONOLINGUAL_BIN} "${MONOLINGUAL_IN}"/{} "${MONOLINGUAL_OUT}"/{}
+}
 
-# Create monolingual data.
-echo "Extracting monolingual data.."
-ls --hide=wet.* "${MONOLINGUAL_IN}" | \
-    parallel ${PARALLEL_OPTIONS} ${MONOLINGUAL_BIN} "${MONOLINGUAL_IN}"/{} "${MONOLINGUAL_OUT}"/{}
+dedupe() {
+    # TODO: Implement sharding capability.
+    # NOTE: Maybe implement that in deduper.
+    # NOTE: Pseudo code for sharder:
+    # Get all monolingual out and pipe it into sharder
+    # use parallel to shard different files on different machines
 
-# TODO: Implement sharding capability.
-# NOTE: Pseudo code for sharder:
-# Get all monolingual out and pipe it into sharder
-# use parallel to shard different files on different machines
+    # Description of the location of all files. We need to escape the asterix
+    # because otherwise we already do file expansion when calling dedupe.sh.
+    local MONO_FILES="${MONOLINGUAL_OUT}/\*/text.{}.gz"
 
-# Description of the location of all files.
-MONO_FILES="${MONOLINGUAL_OUT}/\*/text.{}.gz"
+    echo ""
+    echo "Creating deduped files.."
+    cat "${LANGUAGESFILE}" | \
+        parallel ${PARALLEL_OPTIONS} ${DEDUPED_BIN} ${MONO_FILES} ${DEDUPED_OUT} {}
+}
 
-# Create deduped files.
-echo ""
-echo "Creating deduped files.."
-cat "${LANGUAGESFILE}" | \
-    parallel ${PARALLEL_OPTIONS} ${DEDUPED_BIN} ${MONO_FILES} ${DEDUPED_OUT} {}
+extract_monolingual
+dedupe

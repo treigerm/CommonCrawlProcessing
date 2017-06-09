c3b24c5ab864542ad3ba7463e3d567207a4a6f85
Tim Reichelt <s1547426@sms.ed.ac.uk>
2017-06-09 15:49:15 +0000

    Add parsing of arguments and actual download

--- download.sh
+++ download.sh
@@ -3,12 +3,63 @@
 set -e
 set -o pipefail
 
-setup() {
-    local URL="$1"
+DOWNLOAD_BIN=/home/tim/dev/download/download_wet.sh
 
+# Defaults.
+PARALLELJOBS=8
+
+parse_args() {
+    # Parse arguments. Taken from
+    # https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash.
+    # Use -gt 1 to consume two arguments per pass in the loop (e.g. each
+    # argument has a corresponding value to go with it).
+    # Use -gt 0 to consume one or more arguments per pass in the loop (e.g.
+    # some arguments don't have a corresponding value to go with it such
+    # as in the --default example).
+    while [[ $# -gt 0 ]]; do
+        key="$1"
+
+        case $key in
+            -u|--url)
+                URL="$2"
+                shift # past argument
+                ;;
+            -o|--output-directory)
+                OUTDIR="$2"
+                shift # past argument
+                ;;
+            --sshloginfile)
+                SSHLOGINFILE="$2"
+                shift # past argument
+                ;;
+            -j|--jobs)
+                PARALLELJOBS="$2"
+                shift # past argument
+                ;;
+            -c|--config)
+                CONFIGFILE="$2"
+                shift # past argument
+                ;;
+            *)
+                # unknown option
+                ;;
+        esac
+        shift # past argument or value
+    done
+
+    # TODO: Check wether all necessary command options are provided.
+
+    if [[ ${SSHLOGINFILE} ]]; then
+        PARALLEL_OPTIONS="--nice 19 --progress --sshloginfile ${SSHLOGINFILE} -j ${PARALLELJOBS} --wd ${PWD}"
+    else
+        PARALLEL_OPTIONS="--nice 19 --progress -j ${PARALLELJOBS} --wd ${PWD}"
+    fi
+}
+
+setup() {
     # Make directory for specified crawl
-    mkdir -p ${1}/wet
-    cd ${1}/wet
+    mkdir -p "${OUTDIR}"
+    cd "${OUTDIR}"
 
     # Download path file
     wget "${URL}"
@@ -23,15 +74,29 @@ setup() {
 }
 
 count_downloads() {
-    # TODO: What is the input?
     total=0
     downloaded=0
-    for path in $(cat $1); do
+    for path in $(cat "${OUTDIR}/wet.paths.http"); do
         total=$((total+1))
         FILENAME=$(echo $path | awk ' BEGIN { FS = "/" } { print $(NF-2) "/" $(NF)}')
         if [ -f ${FILENAME}.done ]; then
             downloaded=$((downloaded+1))
         fi
     done
-    # TODO: Echo when not everything is downloaded.
+    difference=$((total-downloaded))
+    if [[ ! $difference -eq 0 ]]; then
+        echo "There are ${difference} files missing/incomplete."
+    done
+}
+
+download() {
+    cat "${OUTDIR}/wet.paths.http" | parallel ${PARALLEL_OPTIONS} ${DOWNLOAD_BIN}
+
+    echo "Count downloaded files.."
+    while [[ count_downloads ]]; do
+        echo "Restarting downloads since there are missing files"
+        cat "${OUTDIR}/wet.paths.http" | parallel ${PARALLEL_OPTIONS} ${DOWNLOAD_BIN}
+        echo "Counting downloaded files.."
+    done
+    echo "All files downloaded"
 }

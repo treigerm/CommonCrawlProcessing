f4ca3abd92b019dd8d1b12a0b77243313822f640
Tim Reichelt <s1547426@sms.ed.ac.uk>
2017-06-13 10:35:53 +0000

    Switch to using getopt command.

--- process_wet.sh
+++ process_wet.sh
@@ -16,6 +16,7 @@ load_config() {
     # Load variables from config file and export the ones which are needed in other
     # scripts.
     source "${1}"
+    # TODO: Rename options.
     MONOLINGUAL_IN="$monolingual_in"
     MONOLINGUAL_OUT="$monolingual_out"
     DEDUPED_OUT="$deduped_out"
@@ -30,67 +31,84 @@ print_help() {
 Usage :
 $APP [options]
 
--h, --help                display help
--min, --monolingual-in    directory with downloaded WET data
--mout, --monolingual-out  directory to output data split according to language
--dout, --dedupe-out       directory for output from deduper
--l,--languagesfile        file which specifies which languages to run the deduper on
---sshloginfile            ssh file for GNU parallel
+-h, --help             display help
+-W, --wet-dir          directory with downloaded WET data
+-M, --monolingual-dir  directory to output data split according to language
+-D, --deduped-dir      directory for output from deduper
+-l,--languagesfile     file which specifies which languages to run the deduper on
+--sshloginfile         ssh file for GNU parallel
 EOF
 }
 
 parse_args() {
+    if [[ $# -eq 0 ]]; then
+        print_help
+        exit 1
+    fi
+
     # Parse arguments. Taken from
     # https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash.
-    # Use -gt 1 to consume two arguments per pass in the loop (e.g. each
-    # argument has a corresponding value to go with it).
-    # Use -gt 0 to consume one or more arguments per pass in the loop (e.g.
-    # some arguments don't have a corresponding value to go with it such
-    # as in the --default example).
-    while [[ $# -gt 0 ]]; do
-        key="$1"
+    local SHORT=hW:M:D:l:j:c:
+    local LONG=help,wet-dir:,monolingual-dir:,deduped-dir:,languagesfile:,sshloginfile:,jobs:,config:
 
-        case $key in
+    # -temporarily store output to be able to check for errors
+    # -activate advanced mode getopt quoting e.g. via “--options”
+    # -pass arguments only via   -- "$@"   to separate them correctly
+    local PARSED=$(getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@")
+    if [[ $? -ne 0 ]]; then
+        exit 1
+    fi
+    # use eval with "$PARSED" to properly handle the quoting
+    eval set -- "$PARSED"
+
+    # now enjoy the options in order and nicely split until we see --
+    while true; do
+        case "$1" in
             -h|--help)
                 print_help
                 exit 0
                 ;;
-            -min|--monolingual-in)
+            -W|--wet-dir)
                 MONOLINGUAL_IN="$2"
-                shift # past argument
+                shift 2
                 ;;
-            -mout|--monolingual-out)
+            -M|--monolingual-dir)
                 MONOLINGUAL_OUT="$2"
-                shift # past argument
+                shift 2
                 ;;
-            -dout|--deduped-out)
+            -D|--deduped-dir)
                 DEDUPED_OUT="$2"
-                shift # past argument
+                shift 2
                 ;;
             -l|--languagesfile)
                 LANGUAGES="$2"
-                shift # past argument
-                ;;
-            -j|--jobs)
-                PARALLELJOBS="$2"
-                shift # past argument
-                ;;
-            --sshloginfile)
-                SSHLOGINFILE="$2"
-                shift # past argument
+                shift 2
                 ;;
             -c|--config)
                 CONFIGFILE="$2"
-                shift # past argument
+                shift 2
+                ;;
+            -j|--jobs)
+                PARALLELJOBS="$2"
+                shift 2
+                ;;
+            --sshloginfile)
+                SSHLOGINFILE="$2"
+                shift 2
+                ;;
+            --)
+                shift
+                break
                 ;;
             *)
-                # unknown option
+                echo "Unexpected error"
+                exit 1
                 ;;
         esac
-        shift # past argument or value
     done
 
     # TODO: Check wether all necessary command options are provided.
+    # TODO: Do list of possible commands.
 
     if [[ ${SSHLOGINFILE} ]]; then
         PARALLEL_OPTIONS="--nice 19 --progress --sshloginfile ${SSHLOGINFILE} -j ${PARALLELJOBS}"

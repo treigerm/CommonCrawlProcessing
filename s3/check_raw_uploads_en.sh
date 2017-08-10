6d76dcd241e8dbd3229d2e0cf092463c909283e4
Tim Reichelt <s1547426@sms.ed.ac.uk>
2017-08-10 20:38:24 +0000

    Clean up s3/

--- s3/check_raw_uploads_en.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-FILE=$(echo "$1" | awk ' BEGIN { FS = "/" } { print $(NF) }')
-OUTFILE="$2"
-
-MD5SUM=$(md5sum "$1" | awk 'BEGIN { FS = " " } {print $1}')
-echo "${MD5SUM} ${FILE}" >> "${OUTFILE}"
--- s3/make_non_en_public.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-RAW_DIR="/fs/vali0/www/data.statmt.org/ngrams/raw"
-LANGS=$(ls ${RAW_DIR}/*.xz | awk 'BEGIN {FS="/"} {print $(NF)}' | cut -d '.' -f 1 | uniq)
-
-for lang in $LANGS; do
-    s3cmd setacl --acl-public -r "s3://web-language-models/ngrams/${lang}/"
-done
